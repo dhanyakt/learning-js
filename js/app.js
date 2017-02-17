893cb726141f11bde92c600ae45a5c34f512de36
Dhanya <dhanyakt@gmail.com>
2017-02-17 15:17:11 -0500

    fix: Fixed refrence error by passing parameter to function

--- js/app.js
+++ js/app.js
@@ -2,21 +2,21 @@
 function myBigFunction() {
     var myValue = 0;
 
-    subFunction1();
-    subFunction2();
-    subFunction3();
+    subFunction1(myValue);
+    subFunction2(myValue);
+    subFunction3(myValue);
 }
 
-function subFunction1() {
-    console.log(myValue);
+function subFunction1(value) {
+    console.log(value);
 }
 
-function subFunction2() {
-    console.log(myValue);
+function subFunction2(value) {
+    console.log(value);
 }
 
-function subFunction3() {
-    console.log(myValue);
+function subFunction3(value) {
+    console.log(value);
 }
 
 myBigFunction();
