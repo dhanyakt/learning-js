b77ae8ffccaf6bb87bb9f07da87e045fe09adb04
Dhanya <dhanyakt@gmail.com>
2017-02-17 15:03:33 -0500

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,22 @@
+
+function myBigFunction() {
+    var myValue = 0;
+
+    subFunction1();
+    subFunction2();
+    subFunction3();
+}
+
+function subFunction1() {
+    console.log(myValue);
+}
+
+function subFunction2() {
+    console.log(myValue);
+}
+
+function subFunction3() {
+    console.log(myValue);
+}
+
+myBigFunction();
